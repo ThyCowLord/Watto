562f931d558b3e5aad9dc5fefe62c6535f72d5b8
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-20 19:21:34 -0500

    Create evilrobots.py

---
+++ evilrobots.py
@@ -0,0 +1,16 @@
+evil = False
+def isevil():
+  if evil == True:
+    kill()
+  else:
+   befriend()
+def befriend():
+  print("Cookies for everyone!")
+  humans == "Nice!"
+  if humans != "Nice!":
+    kill()
+  else:
+    print("I love humans!")
+def kill():
+  print("Installing Windows.... This may take some time...")
+isevil()
