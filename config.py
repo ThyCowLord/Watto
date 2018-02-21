03d68f822fb12bf3b155f27ca2011e061c7a624a
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-20 19:10:51 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ consumer_token - 'xxxxxxx'
 consumer_secret = 'xxxxxxxx'
 email = 'xxxxxx@xxxx.com'
 password = 'xxxxxxxxxx'
-smtp_server = 'smtp.gmail.com' # Default is GMail
+smtp_server = 'smtp.gmail.com' # Default is Gmail
+opt-in = 1 #Change to 0 for opt-in
