e31bf8371d500079019fbb5f61ec34037eaacfdb
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-20 19:26:16 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ consumer_secret = 'xxxxxxxx'
 email = 'xxxxxx@xxxx.com'
 password = 'xxxxxxxxxx'
 smtp_server = 'smtp.gmail.com' # Default is Gmail
-opt-in = 1 #Change to 0 for opt-in
+opt-in = 1 #Change to 0 for opt-in. For reporting 'n stuff
+cat-attack-chance = 1000 # Set your chances of a cat attack!
