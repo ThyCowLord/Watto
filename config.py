2c921e1bf266d8858376c5616452712706779a19
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-20 19:37:42 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ password = 'xxxxxxxxxx'
 smtp_server = 'smtp.gmail.com' # Default is Gmail
 opt-in = 1 #Change to 0 for opt-in. For reporting 'n stuff
 cat-attack-chance = 1000 # Set your chances of a cat attack!
+reddit-opt-in = 1 #Change to 0 to opt in to posting to a subreddit
+subreddit = ''
