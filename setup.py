bbcd08f85cff5592eae7f238d772759f683cdac5
ThyCowLord <34698219+ThyCowLord@users.noreply.github.com>
2018-02-20 20:13:29 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Watto',
+      version='1.0',
+      description='A Twitter answering bot',
+      author='ThyCowLord and impshum',
+      packages=['wikipedia', 'praw', 'tweepy'],
+     )
